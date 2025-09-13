1b674eb2d3074869ced56a8b7fa733e069d7a324
ItsMitlit <albatrosstebex@gmail.com>
2025-09-13 22:44:51 +1200

    Initial commit

---
+++ src/main/java/it/mitl/minecraftbecomeplayers/RegistryHandler.java
@@ -0,0 +1,16 @@
+package it.mitl.minecraftbecomeplayers;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+public class RegistryHandler {
+
+    public static void register() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        //DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientEvents::register);
+
+    }
+
+}
